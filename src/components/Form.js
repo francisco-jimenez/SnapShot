b15334cdc7d11fd4b1c0e55e653bf5999b43868d
Curro Jimenez <curro.jimenez@grapheverywhere.com>
2020-10-23 17:02:25 +0200

    Removing search Term when user is selecting a category

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useLocation, useRouteMatch } from 'react-router-dom'
 
 const Form = ({ handleSubmit, history }) => {
   const [searchEntry, setSearchEntry] = useState("");
@@ -6,6 +7,20 @@ const Form = ({ handleSubmit, history }) => {
   const updateSearchInput = e => {
     setSearchEntry(e.target.value);
   };
+
+  let location = useLocation()
+  let isInSearchPage = useRouteMatch("/search/:searchInput")
+
+
+  useEffect(
+    () => {
+      if (!isInSearchPage) {
+        setSearchEntry('')
+      }
+    },
+    [location]
+  )
+
   return (
     <form
       className="search-form"
