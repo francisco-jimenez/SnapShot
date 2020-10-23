7623082fe4b6b2fa266dff5dc1dee548b05f8199
Curro Jimenez <curro.jimenez@grapheverywhere.com>
2020-10-23 17:36:27 +0200

    Avoid unnecesary API calls by caching images response

--- src/context/PhotoContext.js
+++ src/context/PhotoContext.js
@@ -4,23 +4,41 @@ import { apiKey } from "../api/config";
 export const PhotoContext = createContext();
 
 const PhotoContextProvider = props => {
+  const [cachedImages, setCachedImages] = useState(new Map())
+  const updateCachedImagesMap = (query,images) => {
+    setCachedImages(new Map(cachedImages.set(query.toLowerCase(),images)));
+  }
   const [images, setImages] = useState([]);
   const [loading, setLoading] = useState(true);
-  const runSearch = query => {
+
+  const getImagesFromAPI = (query) => {
     axios
-      .get(
-        `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`
-      )
-      .then(response => {
-        setImages(response.data.photos.photo);
-        setLoading(false);
-      })
-      .catch(error => {
-        console.log(
-          "Encountered an error with fetching and parsing data",
-          error
-        );
-      });
+    .get(
+      `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`
+    )
+    .then(response => {
+      setImages(response.data.photos.photo);
+      updateCachedImagesMap(query.toLowerCase(), response.data.photos.photo)
+      setLoading(false);
+    })
+    .catch(error => {
+      console.log(
+        "Encountered an error with fetching and parsing data",
+        error
+      );
+    });
+  }
+
+  
+  const runSearch = query => {
+    debugger
+    let alreadyCachedImages =  cachedImages.get(query.toLowerCase())
+    if(alreadyCachedImages) {
+      setImages(alreadyCachedImages);
+      setLoading(false);
+    }else {
+      getImagesFromAPI(query)
+    }
   };
   return (
     <PhotoContext.Provider value={{ images, loading, runSearch }}>
